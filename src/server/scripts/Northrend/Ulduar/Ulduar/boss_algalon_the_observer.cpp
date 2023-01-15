fb8352b093c7cb577571d0c630111acd100d82ef
HRLyn <121322041+HRLyn@users.noreply.github.com>
2023-01-15 20:04:37 +0100

    Hotfixes

--- src/server/scripts/Northrend/Ulduar/Ulduar/boss_algalon_the_observer.cpp
+++ src/server/scripts/Northrend/Ulduar/Ulduar/boss_algalon_the_observer.cpp
@@ -4,6 +4,7 @@
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Affero General Public License as published by the
  * Free Software Foundation; either version 3 of the License, or (at your
+
  * option) any later version.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
@@ -1320,7 +1321,7 @@ public:
             if (distance >= 10.0f)
                 SetHitDamage(int32(float(GetHitDamage()) / distance / 1.6));
             else if (distance > 6.0f)
-                SetHitDamage(int32(float(GetHitDamage()) / distance) * 2);
+                SetHitDamage(int32(float(GetHitDamage()) / distance));
         }
 
         void Register() override

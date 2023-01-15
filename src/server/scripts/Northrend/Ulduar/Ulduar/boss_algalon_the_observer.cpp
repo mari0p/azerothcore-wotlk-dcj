e19c3f3bc53b4097c75f7c2cf8fb8997679d96e1
HRLyn <121322041+HRLyn@users.noreply.github.com>
2023-01-15 20:23:37 +0100

    Hotfix

--- src/server/scripts/Northrend/Ulduar/Ulduar/boss_algalon_the_observer.cpp
+++ src/server/scripts/Northrend/Ulduar/Ulduar/boss_algalon_the_observer.cpp
@@ -1320,7 +1320,7 @@ public:
             float distance = GetHitUnit()->GetDistance2d(GetExplTargetDest()->GetPositionX(), GetExplTargetDest()->GetPositionY());
             if (distance >= 10.0f)
                 SetHitDamage(int32(float(GetHitDamage()) / distance / 1.6));
-            else if (distance > 6.0f)
+            else if (distance > 5.0f)
                 SetHitDamage(int32(float(GetHitDamage()) / distance));
         }
 
